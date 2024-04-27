290170bf7ce182f0f2aed8f1b7cdbddacffc0612
Danlois Tovar <danlois.tovar@gmail.com>
2024-04-27 18:16:59 -0300

    🎉 Se agrega función al preloader
    Se agrega función al preloader para evitar el ocultamiento de la barra de desplazamiento en el slider.

--- js/preloader.js
+++ js/preloader.js
@@ -1,8 +1,11 @@
 const preloader = function () {
 	document.addEventListener('DOMContentLoaded', function () {
 		// Muestra el preloader
-		// !lo comente porque ya le di estilo en el css
-		/* document.querySelector(".centrar-loader").style.display = "block"; */
+		// Muestra el preloader
+		/* esta funcion elimina la clase ocultar-body y con esto elimino el error de comportamiento ue al cargar la pagina por mimera vez se oculta la barra de desplazamiento */
+		window.onload = function () {
+			document.body.classList.remove('ocultar-body');
+		};
 		setTimeout(function () {
 			document.querySelector('.centrar-loader').style.display = 'none';
 			document.body.classList.remove('ocultar-body');
